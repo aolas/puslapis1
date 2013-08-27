a429c6650a8a5d11e033e10f58cc02dbccccd2bd
Ramunas <ramunas.daukas@gmail.com>
2013-08-27 03:24:22 -0700

    _config

---
+++ _config.php
@@ -0,0 +1,33 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => '',
+	"database" => '',
+	"path" => '',
+);
+
+MySQLDatabase::set_connection_charset('utf8');
+
+
+// Set the current theme. More themes can be downloaded from
+// http://www.silverstripe.org/themes/
+SSViewer::set_theme('simple');
+
+// Set the site locale
+i18n::set_locale('en_US');
+
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
+
+//debug
+Director::set_environment_type("dev");
+
+//global content
+Object::add_extension('SiteConfig', 'CustomSiteConfig');
